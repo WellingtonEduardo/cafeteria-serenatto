d3f48ef26a054625a7f8d2c26df65c766f9de42f
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-07-26 14:34:25 -0300

    I implemented dynamic data coming from the database

--- src/conexao-bd.php
+++ src/conexao-bd.php
@@ -1,9 +1,3 @@
 <?php
 
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=serenatto",
-    "root",
-    "wellUnited08"
-);
-
-var_dump($pdo);
+$pdo = new PDO('mysql:host=localhost;dbname=serenatto', 'root', '1234');
